a6fba78e72c59a8df890d757c00a85d9a75e1528
Jerrell Mardis <jerrell.mardis@gmail.com>
2016-05-14 15:33:37 -0500

    Added support to launch the Phone app.

--- app/src/main/java/com/contactsmanager/MainActivity.java
+++ app/src/main/java/com/contactsmanager/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.PhoneNumberFormattingTextWatcher;
 import android.text.TextUtils;
 import android.util.Patterns;
 import android.view.View;
@@ -18,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setupEmailListeners();
+
+        setupPhoneListeners();
     }
 
     private void setupEmailListeners() {
@@ -37,6 +40,35 @@ public class MainActivity extends AppCompatActivity {
                         Intent intent = new Intent(Intent.ACTION_SENDTO);
                         intent.setType("text/plain");
                         intent.setData(Uri.parse("mailto:" + email.getText()));
+                        startActivity(intent);
+                    }
+                }
+            });
+        }
+    }
+
+    private void setupPhoneListeners() {
+        final EditText phone = (EditText) findViewById(R.id.phone_edit_text);
+        final View phoneButton = findViewById(R.id.phone_button);
+
+        // set up a listener to format the phone number text as the user types
+        if (phone != null) {
+            phone.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
+        }
+
+        if (phoneButton != null && phone != null) {
+            // set up a click listener to open the phone app with the number provided
+            phoneButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (TextUtils.isEmpty(phone.getText())) {
+                        phone.setError(getString(R.string.phone_error_text));
+                    } else {
+                        phone.setError(null);
+                        // use the ACTION_DIAL action
+                        // using this action doesn't require any additional permissions unlike Intent.ACTION_CALL
+                        Intent intent = new Intent(Intent.ACTION_DIAL);
+                        intent.setData(Uri.parse("tel:" + phone.getText()));
                         startActivity(intent);
                     }
                 }
